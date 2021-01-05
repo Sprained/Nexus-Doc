3cb5c23b45d7ffc2c3afc9a4e2a2eb4f54aba427
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2021-01-05 00:23:19 -0300

    arquivo de conexão

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 include_once __DIR__ . '/vendor/autoload.php';
+include __DIR__ . '/conn/db.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $teste = $_ENV['DB_DATABASE'];
-echo $teste;
+
+$db = new db($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE'], $_ENV['DB_CHARSET']);
+
+$teste = $db->query("SELECT * FROM cidades LIMIT 1")->fetchAll();
+
+print_r($teste);
